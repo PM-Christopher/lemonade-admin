770dea54743ce2af67696b008947674c28edbb02
Dbabalola600 <dami600bab@gmail.com>
2025-05-04 11:50:11 +0100

    updated

--- src/components/global/SideNav.tsx
+++ src/components/global/SideNav.tsx
@@ -2,11 +2,43 @@
 import React from 'react';
 import {pageLinks} from "@/utils/pageLinks";
 import Image from "next/image";
-import {usePathname} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
+import { useAppDispatch } from '@/redux/hook';
+import { logout } from '@/features/authentication/authSlice';
+import { useSelector } from 'react-redux';
+import { updateToastifyReducer } from '@/redux/toastifySlice';
 
 function SideNav({}) {
     const pathname = usePathname();
     const profileActive = pathname === "/profile" || (pathname.startsWith("/profile"));
+    const dispatch = useAppDispatch()
+    const { user, authToken: token } = useSelector((state: any) => state.auth)
+    const router = useRouter()
+    const handleLogout =()=>{
+
+        dispatch(logout({token})).then(res => {
+            if (res.payload.status) {
+                dispatch(
+                    updateToastifyReducer({
+                        show: true,
+                        message: `Logged out`,
+                        type: "success",
+                    })
+                );
+                // redirect user to login
+                router.push("/login")
+            } else {
+                dispatch(
+                    updateToastifyReducer({
+                        show: true,
+                        message: res.payload.message || `Something went wrong`,
+                        type: "error",
+                    })
+                );
+            }
+        })
+
+    }
     return (
         <aside className="flex flex-col gap-[20px] w-64 bg-gray-20 p-4 border-r-[1px] border-r-grey-20 bg-white">
             <div className="text-2xl font-bold mb-8">
@@ -36,7 +68,9 @@ function SideNav({}) {
                 <a href={'/profile'} className={`w-[192px] block  py-[10px] px-[12px] rounded-[12px] text-[14px] font-normal font-sans hover:bg-link-color hover:text-black-light hover:font-semiBold ${
                     profileActive ? "bg-link-color font-semiBold text-black" : "text-text-grey"
                 }`}>Profile</a>
-                <a href="#" className="block py-[10px] px-[12px] text-red-1 rounded-[12px] text-[14px] font-normal font-sans hover:bg-red-1 hover:text-white">Logout</a>
+                <div onClick={()=>{
+                    handleLogout()
+                }} className="block py-[10px] px-[12px] text-red-1 rounded-[12px] text-[14px] font-normal font-sans hover:bg-red-1 hover:text-white">Logout</div>
             </div>
         </aside>
     );
